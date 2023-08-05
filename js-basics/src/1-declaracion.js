1bbf7497dffca0370d4b9bfff185e6152ead5761
Jan Belmar <jano6belmar@gmail.com>
2023-08-05 06:46:53 -0400

    first commit

---
+++ js-basics/src/1-declaracion.js
@@ -0,0 +1,49 @@
+//En informática, los datos son cualquier cosa que tenga sentido para la computadora.//
+//JavaScript proporciona ocho tipos de datos diferentes, los cuales son 
+ 
+ // 
+ //1
+ undefined, 
+ //2
+ null, 
+ //3 boolean:
+ true, false
+
+ //4 string,
+  "esto es un strin"
+
+  //5  symbol, 
+  
+  //6 bigint, 
+  
+  //7 number:
+  1, 2, 3//etc
+ 
+ //8 object.
+  objeto = { 
+    element : 1, 
+    cosa : true, 
+}
+
+//Las variables permiten a los ordenadores almacenar y manipular datos de forma dinámica. 
+//Hacen esto usando una "etiqueta" para apuntar a los datos en lugar de usar los datos en sí. 
+//Cualquiera de los ocho tipos de datos puede almacenarse en una variable.
+
+//Las variables son similares a las variables x, e y que usan en matemáticas, 
+//lo que significa que son un nombre simple para representar los datos a los que queremos hacer referencia. 
+//Las variables de computadora difieren de las variables matemáticas en que pueden almacenar 
+//diferentes valores en diferentes momentos.
+
+//Le decimos a JavaScript que cree o declare una variable poniendo la palabra 
+//clave var delante de ella, así:
+
+var ourName;
+
+//crea una variable llamada ourName. 
+//En JavaScript terminamos las sentencias con punto y coma. 
+//Los nombres de las variables pueden estar formados por números, 
+//letras y $ o _, pero no pueden contener espacios ni empezar con un número.
+
+//Ejercicio
+//Utiliza la palabra clave var para crear una variable llamada myName.
+var myName;
